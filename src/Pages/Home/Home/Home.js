96c646009324de384021ac38b3758181d0972c56
Rabbe <onikmahmudrabbe@gmail.com>
2022-11-13 04:20:36 +0600

    Exceptional treatment page added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Exceptional from '../Exceptional/Exceptional';
 import InfoCards from '../InfoCards/InfoCards';
 import MakeAppointment from '../MakeAppointment/MakeAppointment';
 import Services from '../Services/Services';
@@ -11,6 +12,7 @@ const Home = () => {
             <Banner></Banner>
             <InfoCards></InfoCards>
             <Services></Services>
+            <Exceptional></Exceptional>
             <MakeAppointment></MakeAppointment>
             <Testimonial></Testimonial>
         </div>
