34d9673930ab5d9a2eff782d846cf8a505a39ef9
Rabbe <onikmahmudrabbe@gmail.com>
2022-11-12 13:33:34 +0600

    Make appointment added and added a primary button

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import bg from '../../../assets/images/bg.png'
 import chair from '../../../assets/images/chair.png'
+import PrimaryButton from '../../../components/PrimaryButton/PrimaryButton';
 
 const Banner = () => {
 
@@ -13,11 +14,11 @@ const Banner = () => {
     return (
         <div className="hero" style={bgStyle}>
             <div className="hero-content py-20 flex-col lg:flex-row-reverse">
-                <img src={chair} className="rounded-lg w-1/2 shadow-2xl" alt='' />
+                <img src={chair} className="rounded-lg lg:w-1/2 shadow-2xl" alt='' />
                 <div>
                     <h1 className="text-5xl font-bold">Box Office News!</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary text-white bg-gradient-to-r from-primary to-secondary">Get Started</button>
+                    <PrimaryButton>Get Started</PrimaryButton>
                 </div>
             </div>
         </div>
